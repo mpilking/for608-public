11c83f2d919887d27011eff3fd4754dc04b460fb
Mike Pilkington <mpilking@gmail.com>
2021-08-06 23:41:55 -0500

    More tweaks and error-handling

--- triage_processor_l2t-ts.sh
+++ triage_processor_l2t-ts.sh
@@ -1,48 +1,65 @@
 #!/bin/bash
 # Automate timeline processing for an input triage zip file
+#
+# Usage: triage_processor_l2t-ts.sh /path/to/triage.zip
+#
+# Created by Mike Pilkington for use in SANS FOR608
 # Inspired by https://github.com/ReconInfoSec/velociraptor-to-timesketch
 
-# Usage: triage_processor_l2t-ts.sh /path/to/triage.zip
-
-# Set the $PARENT_DATA_DIR as the location where zips will be processed and plaso files saved
-PARENT_DATA_DIR="/cases/processor"
+# Set the $PROCESSING_DIR as the location where zips will be processed and plaso files saved
+PROCESSING_DIR="/cases/processor"
 
 # Get $TRIAGEZIP to process from positional argument 1. Add $EXTENSION check that it's a .zip file.
 TRIAGEZIP=$1
 EXTENSION="${TRIAGEZIP##*.}"
 
 process_files () {
-    ZIP=$1
-    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] Received $ZIP | tee -a $PARENT_DATA_DIR/$ZIP.log
-    md5sum $PARENT_DATA_DIR/$ZIP | tee -a $PARENT_DATA_DIR/$ZIP.log
-    
+    # Store full input parameter from user as $ZIPFULL
+    ZIPFULL=$1
+    # Extract the filename from user input and store as $ZIP
+    ZIP=$(basename $ZIPFULL)
     # Get system name, or close approximation, based on CyLR and Velociraptor default naming conventions 
     SYSTEM=$(echo $ZIP|cut -d"." -f 1)
-    # Create a unique "timestamped" name to avoid conflicts if identically-named zips are (re)processed
-    TIMESTAMPED_NAME=$SYSTEM-$(date --utc +'%Y%m%dT%H%M%S')Z
+    # Create a unique lowercase timestamped name to avoid conflicts if identically-named zips are (re)processed
+    # (lowercase because Elasticsearch indexes do not support uppercase letters)
+    TIMESTAMPED_NAME=${SYSTEM,,}-$(date --utc +'%Y%m%dt%H%M%S')z
     
-    # Unzip triage data (fuse-zip is a nice alternative, but unzip is more universally installed)
-    echo A | unzip -q $PARENT_DATA_DIR/$ZIP -d $PARENT_DATA_DIR/$TIMESTAMPED_NAME
-    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] Unzipped $ZIP | tee -a $PARENT_DATA_DIR/$ZIP.log
+    # Log and hash new triage zip file for processing
+    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] Received $ZIPFULL | tee -a $PROCESSING_DIR/$TIMESTAMPED_NAME.log
+    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] MD5 hash: $(md5sum $ZIPFULL) | tee -a $PROCESSING_DIR/$TIMESTAMPED_NAME.log
+    
+    # Unzip triage data (fuse-zip is a time- & space-saving alternative, but unzip is more universally installed)
+    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] Unzipping $ZIPFULL to $PROCESSING_DIR/$TIMESTAMPED_NAME | tee -a $PROCESSING_DIR/$TIMESTAMPED_NAME.log
+    echo A | unzip -q $ZIPFULL -d $PROCESSING_DIR/$TIMESTAMPED_NAME
     
     # Run log2timeline and generate Plaso file
-    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Beginning Plaso creation..." | tee -a $PARENT_DATA_DIR/$ZIP.log
-    log2timeline.py --status_view none --parsers 'win7_slow,!filestat' $PARENT_DATA_DIR/$TIMESTAMPED_NAME.plaso $PARENT_DATA_DIR/$TIMESTAMPED_NAME
-    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Plaso creation complete" | tee -a $PARENT_DATA_DIR/$ZIP.log
+    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Beginning Plaso creation of $PROCESSING_DIR/$TIMESTAMPED_NAME.plaso (this typically takes 20 minutes or more)..." | tee -a $PROCESSING_DIR/$TIMESTAMPED_NAME.log
+    log2timeline.py --status_view none --parsers 'win7_slow,!filestat' $PROCESSING_DIR/$TIMESTAMPED_NAME.plaso $PROCESSING_DIR/$TIMESTAMPED_NAME
+    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Plaso file creation finished" | tee -a $PROCESSING_DIR/$TIMESTAMPED_NAME.log
 
     # Run timesketch_importer to send Plaso data to Timesketch
-    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Beginning Timesketch import..." | tee -a $PARENT_DATA_DIR/$ZIP.log
-    timesketch_importer -u sansforensics -p forensics --host http://127.0.0.1 --index_name plaso-$TIMESTAMPED_NAME --timeline_name $TIMESTAMPED_NAME-triage --sketch_name $TIMESTAMPED_NAME-sketch $PARENT_DATA_DIR/$TIMESTAMPED_NAME.plaso
-    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Timesketch import finished" | tee -a $PARENT_DATA_DIR/$ZIP.log
+    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Beginning Timesketch import of $TIMESTAMPED_NAME-triage timeline (this typically takes an hour or more)..." | tee -a $PROCESSING_DIR/$TIMESTAMPED_NAME.log
+    timesketch_importer -u sansforensics -p forensics --host http://127.0.0.1 --index_name plaso-$TIMESTAMPED_NAME --timeline_name $TIMESTAMPED_NAME-triage --sketch_name $TIMESTAMPED_NAME-sketch $PROCESSING_DIR/$TIMESTAMPED_NAME.plaso
+    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Timesketch import finished" | tee -a $PROCESSING_DIR/$TIMESTAMPED_NAME.log
     
-    # Delete unzipped triage data directory
-    rm -r $PARENT_DATA_DIR/$TIMESTAMPED_NAME
-    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Removed unzipped triage directory" | tee -a $PARENT_DATA_DIR/$ZIP.log
+    # Delete unzipped triage data directory, but leave the new plaso file in place (consider zipping it another step)
+    rm -r $PROCESSING_DIR/$TIMESTAMPED_NAME
+    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Removed unzipped triage directory $PROCESSING_DIR/$TIMESTAMPED_NAME" | tee -a $PROCESSING_DIR/$TIMESTAMPED_NAME.log
     
     # Procecessing finished
-    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Processing job finished for $ZIP" | tee -a $PARENT_DATA_DIR/$ZIP.log
+    echo [$(date --utc +'%Y-%m-%d %H:%M:%S') UTC] "Processing job finished for $ZIPFULL" | tee -a $PROCESSING_DIR/$TIMESTAMPED_NAME.log
 }
 
-if [[ $EXTENSION == "zip" ]]; then
-    process_files $TRIAGEZIP
+if [ -f "$TRIAGEZIP" ]; then
+    if [ -w "$PROCESSING_DIR" ]; then
+        if [[ $EXTENSION == "zip" ]]; then
+            process_files $TRIAGEZIP
+        else
+            echo "ERROR: '$TRIAGEZIP' does not appear to be a zip file."
+        fi
+    else
+        echo "ERROR: Processing directory ($PROCESSING_DIR) does not exist or is not writable. Validate the 'PROCESSING_DIR' variable path in the script."
+    fi
+else
+    echo "ERROR: The file '$TRIAGEZIP' does not exist."
 fi
